80818f52eb73c5f25cc7a75cf6e85a6e05d32e91
lestmc <2059038900l@qq.com>
2024-10-26 20:37:29 +0800

    更新 Netlify 配置和 Flask 应用结构

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,9 @@ from config import Config
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder='.')  # 更改模板文件夹为根目录
+    app = Flask(__name__, template_folder='.')
     app.config.from_object(Config)
     
     db.init_app(app)
-    
-    from app.routes import main
-    app.register_blueprint(main)
     
     return app
