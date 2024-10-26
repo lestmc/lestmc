6a5c302b34c6bbdf55b1923a6ad5893229ec11c3
lestmc <2059038900l@qq.com>
2024-10-26 19:32:50 +0800

    Prepare for Zeabur deployment

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///lestmc.db'
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
