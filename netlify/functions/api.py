7697a70f0a591ee0ebd57af523d9708f7f52a4b6
lestmc <2059038900l@qq.com>
2024-10-26 20:32:09 +0800

    将HTML文件移动到根目录并更新配置

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from app import create_app
 from flask_serverless import FlaskServerless
@@ -7,10 +7,24 @@ app = create_app()
 
 @app.route('/')
 def index():
-    return "Welcome to LestMC!"
+    return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return "Hello from Netlify!"
+@app.route('/mods')
+def mods():
+    return render_template('mods.html')
+
+@app.route('/modpacks')
+def modpacks():
+    return render_template('modpacks.html')
+
+@app.route('/resourcepacks')
+def resourcepacks():
+    return render_template('resourcepacks.html')
+
+@app.route('/resources')
+def resources():
+    # 这里你可能需要从数据库获取资源
+    resources = []  # 替换为实际的资源列表
+    return render_template('resources.html', resources=resources)
 
 handler = FlaskServerless(app).handler
